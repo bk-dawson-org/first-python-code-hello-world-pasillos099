fddadc14979be757a0d4d77f15ab7e074d1a802c
pasillos099 <ma.pasillos@dawsoncollege.qc.ca>
2024-08-30 23:36:18 +0000

    first assignment

---
+++ main.py
@@ -0,0 +1,14 @@
+print ("Hello my malo malo")
+
+#pinaka pogi sa balat ng lupa hehe
+student="christine"
+print ("student")
+print (78-9)
+h="20"
+print (h)
+user=input("What's your name?")
+print('Your name is', user)
+firstname=input('Ma christine')
+lastname=input('Pasillos')
+name='firstname'+' '+'lastname'
+print(name)
---
+++ math.py
@@ -0,0 +1,6 @@
+print(2**5)
+print(5+2-1*4/8//9**2)
+celsius text=input("0")
+celsius=float(celsius text)
+fahr=celcius text*9/5+32
+print("fahr")
